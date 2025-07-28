7547a35ab3f76d2168ed03ae329ca1f0bc9e6207
Mola Pahnadayan <mola.mp@gmail.com>
2025-07-28 14:25:06 +0330

    Add api deepseek to config

--- aiatconfig.py
+++ aiatconfig.py
@@ -1,5 +1,6 @@
 # aiatconfig.py
 import os
+from PySide6.QtCore import QSettings
 
 class _AiAtConfig:
     _instance = None
@@ -9,6 +10,7 @@ class _AiAtConfig:
             raise RuntimeError("AiatConfig is a singleton - use AiatConfig.get_instance()")
         self.conf_dir = None
         self.conf_dbdir = None
+        self.deepseek_api_key = None
 
     @classmethod
     def get_instance(cls):
@@ -18,9 +20,14 @@ class _AiAtConfig:
 
     def set_conf_dir(self, conf_dir):
         self.conf_dir = conf_dir
+        self.initialize_config()
 
     def get_conf_dir(self):
         return self.conf_dir
+
+    def initialize_config(self): # add this method
+        settings = QSettings(os.path.join(self.conf_dir, "settings.ini"), QSettings.IniFormat)
+        self.deepseek_api_key = settings.value("DeepSeek/API_KEY")
 
     def set_conf_dbdir(self, dbdir):
         self.conf_dbdir = dbdir
@@ -32,6 +39,12 @@ class _AiAtConfig:
         if not self.conf_dbdir:
             return None  # Return None instead of raising an error
         return os.path.join(self.conf_dbdir, "database.db")
+
+    def set_deepseek_api_key(self, key): # add this method
+        self.deepseek_api_key = key
+
+    def get_deepseek_api_key(self): # add this method
+        return self.deepseek_api_key
 
 # Global singleton instance
 AiAtConfig = _AiAtConfig.get_instance()
