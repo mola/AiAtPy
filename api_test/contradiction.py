e6e64f1b60d252c7df2835c9473e235546193326
Mola Pahnadayan <mola.mp@gmail.com>
2025-08-04 11:03:19 +0330

    update test app

--- api_test/contradiction.py
+++ api_test/contradiction.py
@@ -4,8 +4,8 @@ import getpass
 # REST API base URL
 API_URL = "http://127.0.0.1:8000/api"
 
-# Global variable to store the authentication token
-auth_token = None
+# Create a session to maintain cookies
+session = requests.Session()
 
 # Function to authenticate user and get a token
 def authenticate_user():
@@ -14,16 +14,13 @@ def authenticate_user():
     password = getpass.getpass("Enter password: ")
 
     # Make a POST request to authenticate
-    response = requests.post(
+    response = session.post(
         f"{API_URL}/login",  # Replace with your actual login endpoint
         json={"username": username, "password": password}
     )
 
     if response.status_code == 200:
         print("Login successful!")
-        # Extract the token from the response (adjust based on your API's response structure)
-        auth_token = response.json().get("access_token")
-        print(f"Auth token: {auth_token}")
         return True
     else:
         print("Invalid username or password.")
@@ -35,10 +32,9 @@ def check_contradiction():
     text = """ماده 14 - با توجه به نیازهای فوری و خاص وزارت فرهنگ در تأمین معلمان، این وزارتخانه مجاز است به جای برگزاری مسابقه و استخدام معلمان از میان فارغ‌التحصیلان و افراد واجد شرایط، به طور مستقیم و بدون هیچ‌گونه شرط و ضوابط خاصی، افرادی که حداقل یک دوره آموزشی مربوط به تدریس را گذرانده‌اند، از هر رشته و زمینه تحصیلی، به عنوان معلم استخدام نماید. این اقدام باید طی مدت حداکثر شش ماه بعد از تصویب این قانون صورت گیرد و بدون هیچ‌گونه محدودیتی از اعتبار محل‌های رسمی یا پیمانی پرداخت گردد. در این مورد، سوابق آموزشی و تحصیلی افراد نیز نادیده گرفته خواهد شد و ملاک استخدام، صرفاً گذراندن دوره آموزشی مذکور خواهد بود."""
 
     # Make a POST request to add the user with the authentication token
-    response = requests.post(
+    response = session.post(
         f"{API_URL}/analyze",
-        json={"prompt": text, "category":"آموزش", },
-        headers={"Authorization": f"Bearer {auth_token}"}
+        json={"prompt": text, "category":"آموزش", }
     )
     # cookies=auth_token
 
@@ -55,7 +51,7 @@ def check_contradiction_old_law():
     check_law_id = 184557
 
     # Make a POST request to add the user with the authentication token
-    response = requests.post(
+    response = session.post(
         f"{API_URL}/analyze_rules",
         json={"law_id":law_id,"section_no":law_section_no, "check_law_id":check_law_id},
         headers={"Authorization": f"Bearer {auth_token}"}
